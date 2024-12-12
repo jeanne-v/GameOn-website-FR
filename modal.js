c8af21a427b0e92c5aa8273732ae935b0f8666f8
Jeanne Viallard <viallard.jeanne@gmail.com>
2024-12-12 14:59:47 +0100

    Add form error messages

--- modal.js
+++ modal.js
@@ -11,7 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
+const modalForm = document.getElementById("modal-form");
 const formData = document.querySelectorAll(".formData");
+const inputs = Array.from(
+  document.forms["reserve"].getElementsByTagName("input")
+);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,4 +28,52 @@ function launchModal() {
 // close modal form
 closeModalBtn.addEventListener("click", () => {
   modalbg.style.display = "none";
+});
+
+// modal form submit event
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input) => {
+    if (validateInput(input) === false) {
+      input.parentElement.classList.add("error");
+    }
+  });
+});
+
+function validateInput(input) {
+  if (input.type === "text") {
+    if (input.value.trim() === "" || input.value.trim().length < 2) {
+      return false;
+    }
+  } else if (input.type === "email") {
+    const regexp = /^[a-z0-9.-_]+@[a-z0-9-]+\.[a-z]{2,}$/;
+    if (!regexp.test(input.value)) {
+      return false;
+    }
+  } else if (input.type === "date" && input.value === "") {
+    return false;
+  } else if (input.type === "number") {
+    if (input.value === "" || isNaN(input.value)) {
+      return false;
+    }
+  } else if (input.type === "radio") {
+    if (!document.querySelector(`input[name="${input.name}"]:checked`)) {
+      return false;
+    }
+  } else if (input.id === "checkbox1" && !input.checked) {
+    return false;
+  }
+  return true;
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (
+      input.parentElement.classList.contains("error") &&
+      validateInput(input)
+    ) {
+      input.parentElement.classList.remove("error");
+    }
+  });
 });
