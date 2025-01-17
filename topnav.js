3a8e8ec9b5c30bd9dfa9af64711ebbdaeb0186eb
Jeanne Viallard <viallard.jeanne@gmail.com>
2025-01-17 12:13:57 +0100

    Extract topbar js from html

--- topnav.js
+++ topnav.js
@@ -1,3 +1,9 @@
+const navbarIcon = document.querySelector(".main-navbar .icon");
+
+navbarIcon.addEventListener("click", () => {
+  editNav();
+});
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
